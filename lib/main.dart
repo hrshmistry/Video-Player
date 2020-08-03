306a77ab79b1e6e7b46a8baa947aba5841b8a4d3
hrshmistry <harsh241299mistry@gmail.com>
2020-08-04 03:29:12 +0530

    initial code commit

---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:video_app/videos_list.dart';
+import 'package:video_player/video_player.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+int status = 0;
+AudioPlayer player = AudioPlayer();
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.purple[50],
+        appBar: AppBar(
+          leading: Icon(
+            Icons.play_circle_filled,
+            size: 45,
+          ),
+          backgroundColor: Colors.deepPurple,
+          title: Text("Music Player"),
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.new_releases),
+                onPressed: () {
+                  Fluttertoast.showToast(
+                      msg: "New features coming soon!",
+                      toastLength: Toast.LENGTH_LONG,
+                      gravity: ToastGravity.BOTTOM,
+                      timeInSecForIosWeb: 1,
+                      backgroundColor: Colors.deepPurple,
+                      textColor: Colors.white,
+                      fontSize: 16.0);
+                })
+          ],
+        ),
+        body: Stack(children: <Widget>[
+          ListView(
+            children: <Widget>[
+              VideosList(
+                videoPlayerController: VideoPlayerController.asset(
+                  'videos/Ansible.MP4',
+                ),
+                looping: true,
+              ),
+              VideosList(
+                videoPlayerController: VideoPlayerController.asset(
+                  'videos/Specialist_In_Python.MP4',
+                ),
+                looping: true,
+              ),
+              VideosList(
+                videoPlayerController: VideoPlayerController.asset(
+                  'videos/AnsiblePro.MP4',
+                ),
+                looping: true,
+              ),
+              VideosList(
+                videoPlayerController: VideoPlayerController.network(
+                  'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+                ),
+                looping: true,
+              ),
+              VideosList(
+                videoPlayerController: VideoPlayerController.asset(
+                  'videos/OpenShift.MP4',
+                ),
+                looping: true,
+              ),
+              VideosList(
+                videoPlayerController: VideoPlayerController.asset(
+                  'videos/Flutter.MP4',
+                ),
+                looping: true,
+              ),
+            ],
+          ),
+        ]),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.audiotrack),
+          elevation: 15,
+          backgroundColor: Colors.deepPurple,
+          onPressed: () async {
+            if (status == 1) {
+              status = await player.stop();
+              status = 0;
+            } else {
+              String filePath = await FilePicker.getFilePath();
+              status = await player.play(filePath, isLocal: true);
+              //also can be played from the assets...
+              //but users must have choices so local file is used!!
+            }
+          },
+        ));
+  }
+}
---
+++ lib/videos_list.dart
@@ -0,0 +1,61 @@
+import 'package:chewie/chewie.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideosList extends StatefulWidget {
+  final VideoPlayerController videoPlayerController;
+  final bool looping;
+
+  const VideosList(
+      {Key key, @required this.videoPlayerController, this.looping})
+      : super(key: key);
+
+  @override
+  _VideosListState createState() => _VideosListState();
+}
+
+class _VideosListState extends State<VideosList> {
+  ChewieController videosController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    videosController = ChewieController(
+      videoPlayerController: widget.videoPlayerController,
+      aspectRatio: 16 / 9,
+      autoInitialize: true,
+      looping: widget.looping,
+      errorBuilder: (context, errorMessage) {
+        return Center(child: progressBar()
+            // Text(
+            //   errorMessage,
+            //   style: TextStyle(color: Colors.white),
+            // ),
+            );
+      },
+    );
+  }
+
+  Widget progressBar() {
+    return CircularProgressIndicator();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Chewie(
+        controller: videosController,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    // IMPORTANT to dispose of all the used resources
+    // widget.videoPlayerController.dispose();
+    videosController.dispose();
+  }
+}
